5ae03f6de2f0e92df60801fd33c4b2573cc104ef
David Philipson <dphilipson@palantir.com>
2016-12-24 05:44:47 -0800

    Tracks closest point on mouse hover

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ import Graph, {
     SimpleEdge,
     SimpleNode,
 } from "graphs-and-paths";
-
 const d3 = { ...d3Selection, ...d3Request, ...d3Shape, ...d3Transition };
 
 interface GraphData {
@@ -18,32 +17,46 @@ interface GraphData {
     edges: SimpleEdge[];
 }
 
+interface Conversions {
+    graphToSvg: Conversion;
+    svgToGraph: Conversion;
+}
+
+interface Conversion {
+    (location: Location): Location;
+}
+
 // Should be kept up to date with index.css.
 const SVG_WIDTH = 960;
 const SVG_HEIGHT = 500;
 
-const svg = d3.select("#demo");
+const svgElement = document.getElementById("demo") as HTMLElement;
+const svg = d3.select(svgElement);
 
 d3.json("data/twin_peaks_1_mile.json", (error, data: GraphData) => {
     const { nodes, edges } = data;
     const graph = Graph.create(nodes, edges).withClosestPointMesh(25);
-    renderGraph(graph);
+    const conversions = makeCoordinateConversionFunctions(graph);
+    const { graphToSvg, svgToGraph } = conversions;
+    renderGraph(graph, graphToSvg);
+    setUpHoverListener(graph, conversions);
 });
 
-function renderGraph(graph: Graph): void {
-    const graphToSvgCoordinates = makeGraphToSvgCoordinates(graph);
-    const circle = svg.selectAll("circle")
+function renderGraph(graph: Graph, graphToSvg: Conversion): void {
+    const circle = svg.selectAll(".node")
         .data(graph.getAllNodes());
     circle.enter().append("circle")
+        .classed("node", true)
         .attr("r", 2.5)
         .merge(circle)
-        .attr("cx", (node) => graphToSvgCoordinates(node.location).x)
-        .attr("cy", (node) => graphToSvgCoordinates(node.location).y);
+        .attr("cx", (node) => graphToSvg(node.location).x)
+        .attr("cy", (node) => graphToSvg(node.location).y);
     circle.exit().remove();
 
-    const path = svg.selectAll("path")
+    const path = svg.selectAll(".edge")
         .data(graph.getAllEdges());
     path.enter().append("path")
+        .classed("edge", true)
         .attr("stroke", "black")
         .attr("stroke-width", 3)
         .attr("stroke-linecap", "round")
@@ -53,13 +66,11 @@ function renderGraph(graph: Graph): void {
         .attr("d", (edge) =>
             d3.line<Location>()
                 .x((location) => location.x)
-                .y((location) => location.y)(edge.locations.map(graphToSvgCoordinates)));
+                .y((location) => location.y)(edge.locations.map(graphToSvg)));
     path.exit().remove();
 }
 
-
-
-function makeGraphToSvgCoordinates(graph: Graph) {
+function makeCoordinateConversionFunctions(graph: Graph): Conversions {
     const minX = Math.min(...graph.getAllNodes().map((node) => node.location.x));
     const maxX = Math.max(...graph.getAllNodes().map((node) => node.location.x));
     const minY = Math.min(...graph.getAllNodes().map((node) => node.location.y));
@@ -71,12 +82,51 @@ function makeGraphToSvgCoordinates(graph: Graph) {
     const graphToSvgRatio = graphAspectRatio > svgAspectRatio
         ? graphHeight / SVG_HEIGHT
         : graphWidth / SVG_WIDTH;
+    const svgCenter = { x: SVG_WIDTH / 2, y: SVG_HEIGHT / 2 };
+    return {
+        graphToSvg(location: Location): Location {
+            return {
+                x: svgCenter.x + location.x / graphToSvgRatio,
+                y: svgCenter.y + location.y / graphToSvgRatio,
+            };
+        },
 
-    return (location: Location): Location => {
-        const svgCenter = { x: SVG_WIDTH / 2, y: SVG_HEIGHT / 2 };
-        return {
-            x: svgCenter.x + location.x / graphToSvgRatio,
-            y: svgCenter.y + location.y / graphToSvgRatio,
-        };
+        svgToGraph(location: Location): Location {
+            return {
+                x: (location.x - svgCenter.x) * graphToSvgRatio,
+                y: (location.y - svgCenter.y) * graphToSvgRatio,
+            }
+        },
     };
+}
+
+function setUpHoverListener(graph: Graph, conversions: Conversions): void {
+    const { graphToSvg, svgToGraph } = conversions;
+    svg.append("circle")
+        .classed("closest-point-highlight", true)
+        .attr("r", 4);
+    const updateClosestPointHighlight = (location: Location | null) => {
+        const highlight = svg.select(".closest-point-highlight")
+            .attr("visibility", location ? "visible" : "hidden");
+        if (location) {
+            const svgLocation = graphToSvg(location);
+            highlight.attr("cx", svgLocation.x)
+                .attr("cy", svgLocation.y);
+        }
+    }
+    updateClosestPointHighlight(null);
+
+    svgElement.onmousemove = (event) => {
+        const { clientX, clientY } = event;
+        const { left, top } = svgElement.getBoundingClientRect();
+        const svgLocation: Location = {
+            x: clientX - left | 0,
+            y: clientY - top | 0,
+        };
+        const graphLocation = svgToGraph(svgLocation);
+        const closestPoint = graph.getLocation(graph.getClosestPoint(graphLocation));
+        updateClosestPointHighlight(closestPoint);
+    };
+
+    svgElement.onmouseleave = () => updateClosestPointHighlight(null);
 }
