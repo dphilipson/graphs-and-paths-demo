cdcc61ed638e79a4a0a185ae937a3742ddb8c52b
David Philipson <dphilipson@palantir.com>
2016-12-28 01:09:17 -0800

    Animates out the paths on release

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as d3Transition from "d3-transition";
 import Graph, {
     EdgePoint,
     Location,
+    Path,
     SimpleEdge,
     SimpleNode,
 } from "graphs-and-paths";
@@ -39,7 +40,8 @@ function main(): void {
         const { nodes, edges } = data;
         const graph = Graph.create(nodes, edges).withClosestPointMesh(25);
         const { graphToSvg, svgToGraph } = makeCoordinateConversionFunctions(graph);
-        runDemo(svgElement, graph, svgToGraph, graphToSvg);
+        const pathExitSpeed = getPathExitSpeed(svgToGraph);
+        runDemo(svgElement, graph, svgToGraph, graphToSvg, pathExitSpeed);
     });
 }
 
@@ -73,14 +75,25 @@ function makeCoordinateConversionFunctions(graph: Graph) {
     };
 }
 
+function getPathExitSpeed(svgToGraph: (location: Location) => Location) {
+    // A path crossing the whole viewport on the diagonal should take this much time to disapear.
+    const diagonalExitTime = 1000;
+    const { x, y } = svgToGraph({ x: 0, y: 0 });
+    // Takes advantage of the fact that (0, 0) in graph coordinates is at the center of the svg.
+    const diagonalLength = 2 * Math.sqrt(x * x + y * y);
+    return diagonalLength / diagonalExitTime;
+}
+
 function runDemo(
     svgElement: HTMLElement,
     graph: Graph,
     svgToGraph: (location: Location) => Location,
     graphToSvg: (location: Location) => Location,
+    pathExitSpeed: number,
 ) {
     let activePathStart: EdgePoint | null = null;
     let closestPoint: EdgePoint | null = null;
+    let exitingPaths: Path[] = [];
 
     renderGraph();
 
@@ -90,6 +103,7 @@ function runDemo(
 
     update();
     setUpMouseListeners();
+    startAnimationTicks();
 
     function renderGraph(): void {
         const edgeSelection = d3.select(svgElement).selectAll(".edge")
@@ -136,6 +150,7 @@ function runDemo(
         updateClosestPointHighlight();
         updateActivePathHighlight();
         updateActivePath();
+        updateExitingPaths();
     }
 
     function updateClosestPointHighlight(): void {
@@ -172,6 +187,21 @@ function runDemo(
         }
     }
 
+    function updateExitingPaths(): void {
+        const exitingPathSelection = d3.select(svgElement).selectAll(".exiting-path")
+            .data(exitingPaths);
+        exitingPathSelection.enter().append("path")
+            .classed("exiting-path", true)
+            .attr("stroke", "#D9822B")
+            .attr("stroke-width", 6)
+            .attr("stroke-linecap", "round")
+            .attr("stroke-linejoin", "round")
+            .attr("fill", "none")
+            .merge(exitingPathSelection)
+            .attr("d", (path) => pathGenerator(path.locations.map(graphToSvg)));
+        exitingPathSelection.exit().remove();
+    }
+
     function setUpMouseListeners(): void {
         svgElement.onmousedown = (event) => {
             activePathStart = closestPointForEvent(event);
@@ -182,6 +212,9 @@ function runDemo(
             update();
         }
         svgElement.onmouseup = () => {
+            if (activePathStart && closestPoint) {
+                exitingPaths.push(graph.getShortestPath(activePathStart, closestPoint));
+            }
             activePathStart = null;
             update();
         }
@@ -201,5 +234,31 @@ function runDemo(
         };
         const graphLocation = svgToGraph(svgLocation);
         return graph.getClosestPoint(graphLocation);
+    }
+
+    function startAnimationTicks(): void {
+        startAnimationTicksFromTime(Date.now());
+    }
+
+    function startAnimationTicksFromTime(lastTime: number): void {
+        window.requestAnimationFrame((time) => {
+            advanceExitingPaths(time - lastTime);
+            startAnimationTicksFromTime(time);
+        });
+    }
+
+    function advanceExitingPaths(dTime: number): void {
+        if (exitingPaths.length > 0) {
+            const distanceGained = dTime * pathExitSpeed;
+            const newPaths: Path[] = [];
+            exitingPaths.forEach((path) => {
+                const newPath = graph.advanceAlongPath(path, distanceGained);
+                if (newPath.length > 0) {
+                    newPaths.push(newPath);
+                }
+            });
+            exitingPaths = newPaths;
+            update();
+        }
     }
 }
